f4918fcdf70b2ff51debaa73def6177c7b796935
Christian <mugidota@hotmail.com>
2022-12-21 12:05:20 +0100

    Add final changes to App

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,119 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <div class="search-container" :class="{ 'slide-top': showsData && showsData.length }">
+      <router-link to="/">
+        <img src="./assets/images/tvm-header-logo.png" alt="">
+      </router-link>
+      <Search v-if="showsData" />
+    </div>
+
+    <transition name="scale">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from '@vue/composition-api';
+import Search from "./components/Search/Search.vue";
 
-@Component({
+export default defineComponent({
+  name: "App",
   components: {
-    HelloWorld,
+    Search,
   },
+  data() {
+    return {
+      shows: [] as Array<object>,
+      cast: [] as Array<object>
+    }
+  },
+  methods: {
+    onRefresh() {
+      if (this.$route.name !== 'Home') {
+        this.$router.push({ name: 'Home' }).catch((err) => { console.log(err) });
+      }
+    }
+  },
+  computed: {
+    showsData(): Array<object> {
+      return this.$store.getters.getShows;
+    },
+    castData(): Array<object> {
+      return this.$store.getters.getCast;
+    },
+  },
+  mounted() {
+    this.onRefresh();
+  },
+  updated() {
+    this.shows = this.showsData;
+    this.cast = this.castData;
+  }
 })
-export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "assets/scss/_base.scss";
+
+.app {
+  .search-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    top: 40vh;
+    position: relative;
+    transition: all 0.1s ease;
+    padding: 15px;
+
+    a {
+      padding-bottom: 100px;
+    }
+  }
+
+  .slide-top {
+    top: 0;
+    position: sticky;
+    background-color: $gray;
+    box-shadow: -1px 1px 10px 0px #00000094;
+    z-index: 100;
+
+    a {
+      padding: 20px 100px;
+    }
+  }
+}
+
+@media only screen and (min-width: $medium) {
+  .app {
+    .search-container {
+      a {
+        margin: auto 100px;
+        padding-bottom: 100px;
+
+        img {
+          padding: 0;
+          width: 300x;
+        }
+      }
+    }
+
+    .slide-top {
+      display: flex;
+      flex-direction: row;
+      top: 0;
+      position: sticky;
+      background-color: $gray;
+      box-shadow: -1px 1px 10px 0px #00000094;
+
+      a {
+        padding: 0;
+
+        img {
+          padding: 0;
+        }
+      }
+    }
+  }
 }
 </style>
